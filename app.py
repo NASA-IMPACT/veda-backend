7df6a2ffcf308f724e58f380b81b6c5b819fc6f8
anayeaye <alexandra@developmentseed.org>
2021-12-23 11:46:49 -0700

    hack in the stac-api lambda from eo-api

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from constructs import Construct
 
 from network.infrastructure.vpc import VpcConstruct
 from database.infrastructure.rds import RdsConstruct
+from stac_api.infrastructure.lambda_function import StacApiLambdaConstruct
 
 identifier = os.getenv("IDENTIFIER").capitalize()
 
@@ -39,7 +40,12 @@ database = RdsConstruct(
     vpc.vpc,
 )
 
-
+stac_api = StacApiLambdaConstruct(
+    delta_stack,
+    f"StacApi{identifier}",
+    vpc.vpc,
+    database,
+)
 
 app.synth()
 
---
+++ stac_api/infrastructure/lambda_function.py
@@ -0,0 +1,53 @@
+import os
+from aws_cdk import (
+    aws_apigateway,
+    aws_lambda,
+    CfnOutput
+)
+from constructs import Construct
+
+from database.infrastructure.custom_resource.bootstrapper import BootstrapPgStac
+
+class StacApiLambdaConstruct(Construct):
+    def __init__(
+        self, 
+        scope: Construct,
+        construct_id: str, 
+        vpc,
+        database,
+        code_dir: str = "./",
+        **kwargs
+    ) -> None:
+        super().__init__(scope, construct_id)
+
+        lambda_function = aws_lambda.Function(
+            self,
+            "LambdaFunction",
+            handler="handler.handler",
+            runtime=aws_lambda.Runtime.PYTHON_3_8,
+            code=aws_lambda.Code.from_docker_build(
+                path=os.path.abspath("./"),
+                file="stac_api/infrastructure/runtime/Dockerfile",
+            ),
+            vpc=vpc,
+            # allow_public_subnet=True,
+            # memory_size=eostac_settings.memory,
+            # timeout=core.Duration.seconds(eostac_settings.timeout),
+            # environment=eostac_settings.env or {},
+        )
+        
+        # # TODO 
+        # database.secret.grant_read(lambda_function)
+    
+        # # lambda_function.add_environment(key="TITILER_ENDPOINT", value=raster_api.url)
+        # database.secret.grant_read(lambda_function)
+        # database.connections.allow_from(lambda_function, port_range=aws_ec2.Port.tcp(5432))
+
+        
+        stac_api = aws_apigateway.LambdaRestApi(
+            self,
+            "StacEndpoint",
+            handler=lambda_function,
+        )
+        
+        CfnOutput(self, "DeltaBackendStacApi", value=stac_api.url)
