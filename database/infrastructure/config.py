1c692fec72e68dc9b4f9ac35dbcef1f480c450e7
Leo Thomas <leo.thomas@mail.mcgill.ca>
2022-03-02 17:09:23 -0500

    Updated database user name

--- database/infrastructure/config.py
+++ database/infrastructure/config.py
@@ -1,17 +1,18 @@
+"""Delta-backend database construct configuration."""
 import pydantic
 
 
 class deltaDBSettings(pydantic.BaseSettings):
-    """Application settings"""
+    """Application settings."""
 
     dbname: str = "postgis"
-    user: str = "eostac"
+    user: str = "delta"
 
     # Define PGSTAC VERSION
     pgstac_version: str
 
     class Config:
-        """model config"""
+        """model config."""
 
         env_file = "deployment/.env"
         env_prefix = "DELTA_DB_"
