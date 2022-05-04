9d4c8433c0ace9ea6852c7911a4c13b6d9d8f295
anayeaye <alexandra@developmentseed.org>
2022-05-04 16:43:36 -0600

    remove lru cache stac api config

--- raster_api/runtime/src/config.py
+++ raster_api/runtime/src/config.py
@@ -3,7 +3,6 @@
 import base64
 import json
 import os
-from functools import lru_cache
 
 import boto3
 import pydantic
@@ -65,7 +64,7 @@ def get_secret_dict(secret_name: str):
             )
 
 
-class _ApiSettings(pydantic.BaseSettings):
+class ApiSettings(pydantic.BaseSettings):
     """API settings"""
 
     name: str = "delta-raster"
@@ -75,6 +74,8 @@ class _ApiSettings(pydantic.BaseSettings):
 
     # MosaicTiler settings
     enable_mosaic_search: bool = False
+
+    pgstac_secret_arn: str
 
     @pydantic.validator("cors_origins")
     def parse_cors_origin(cls, v):
@@ -86,18 +87,6 @@ class _ApiSettings(pydantic.BaseSettings):
 
         env_file = ".env"
         env_prefix = "DELTA_RASTER_"
-
-
-@lru_cache()
-def ApiSettings() -> _ApiSettings:
-    """
-    This function returns a cached instance of the APISettings object.
-    Caching is used to prevent re-reading the environment every time the API settings are used in an endpoint.
-    If you want to change an environment variable and reset the cache (e.g., during testing), this can be done
-    using the `lru_cache` instance method `get_api_settings.cache_clear()`.
-    From https://github.com/dmontagu/fastapi-utils/blob/af95ff4a8195caaa9edaa3dbd5b6eeb09691d9c7/fastapi_utils/api_settings.py#L60-L69
-    """
-    return _ApiSettings()
 
 
 def put_postgres_env(secretsmanager_arn: str) -> None:
