1121c842f25a5e7788dc9f233a047e1696671686
anayeaye <alexandra@developmentseed.org>
2022-05-24 15:58:43 -0600

    linting

--- raster_api/runtime/src/datasetparams.py
+++ raster_api/runtime/src/datasetparams.py
@@ -1,18 +1,20 @@
-from titiler.core import dependencies
+"""From eoAPI datasetparams edl_auth branch https://github.com/NASA-IMPACT/eoAPI/blob/edl_auth/src/eoapi/raster/eoapi/raster/datasetparams.py"""
+import math
 from dataclasses import dataclass
-import numpy
-from fastapi import Query
+from typing import Optional, Tuple
 
-from typing import Tuple, Optional
+import numpy
+
+from fastapi import Query
+from titiler.core import dependencies
 
 # https://github.com/cogeotiff/rio-tiler/blob/master/rio_tiler/reader.py#L35-L37
 
 # From eoAPI datasetparams edl_auth branch https://github.com/NASA-IMPACT/eoAPI/blob/edl_auth/src/eoapi/raster/eoapi/raster/datasetparams.py
 
-import math
-
 
 def swir(data, mask) -> Tuple[numpy.ndarray, numpy.ndarray]:
+    """SWIR"""
     low_value = math.e
     high_value = 255
 
@@ -22,9 +24,7 @@ def swir(data, mask) -> Tuple[numpy.ndarray, numpy.ndarray]:
     data = numpy.log(data)
     data[numpy.where(data <= low_threshold)] = low_value
     data[numpy.where(data >= high_threshold)] = high_value
-    indices = numpy.where(
-        (data > low_value) & (data < high_value)
-    )
+    indices = numpy.where((data > low_value) & (data < high_value))
     data[indices] = (
         high_value * (data[indices] - low_threshold) / (high_threshold - low_threshold)
     )
@@ -38,9 +38,12 @@ pp_methods = {
 
 @dataclass
 class DatasetParams(dependencies.DatasetParams):
+    """Post processing parameters for map layers"""
+
     post_process: Optional[str] = Query(None, description="Post Process Name.")
 
     def __post_init__(self):
+        """."""
         super().__post_init__()
 
         if self.post_process is not None:
