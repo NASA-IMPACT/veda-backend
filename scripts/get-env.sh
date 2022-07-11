1e121bc399c6fdc5cb03cdd67db9288f1d16b366
anayeaye <alexandra@developmentseed.org>
2022-07-11 14:38:54 -0600

    try set github env variables

--- scripts/get-env.sh
+++ scripts/get-env.sh
@@ -2,5 +2,5 @@
 # Use this script to load environment variables for a deployment from AWS Secrets
 
 for s in $(aws secretsmanager get-secret-value --secret-id $1 --query SecretString --output text | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); do
-    export "$s"
+    echo "$s" >> $GITHUB_ENV
 done
