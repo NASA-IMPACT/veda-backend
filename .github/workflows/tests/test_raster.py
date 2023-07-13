ea154c9ab1fc063bd0044294736d6a2fb6347bdd
ranchodeluxe <greg.corradini@gmail.com>
2023-07-13 10:36:45 -0700

    make it fail now

--- .github/workflows/tests/test_raster.py
+++ .github/workflows/tests/test_raster.py
@@ -27,12 +27,6 @@ def test_mosaic_api_blackmarble():
 
     searchid = resp.json()["searchid"]
 
-    resp = httpx.get(f"{raster_endpoint}/mosaic/{searchid}/-85.6358,36.1624/assets")
-    assert resp.status_code == 200
-    assert len(resp.json()) == 1
-    assert list(resp.json()[0]) == ["id", "bbox", "assets", "collection"]
-    assert resp.json()[0]["id"] == "20200307aC0852700w361200"
-
     resp = httpx.get(f"{raster_endpoint}/mosaic/{searchid}/0/0/0/assets")
     assert resp.status_code == 200
     assert len(resp.json()) == 1
