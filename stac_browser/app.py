d5ea0ee7f3340d78495991918c9662c7720f6833
emileten <e.tenezakis@gmail.com>
2023-09-08 18:55:04 +0900

    app

--- stac_browser/app.py
+++ stac_browser/app.py
@@ -4,7 +4,8 @@
 
 import os
 
-from aws_cdk import App, Stack, Tags
+from aws_cdk import App, RemovalPolicy, Stack, Tags
+from aws_cdk import aws_s3 as s3
 from constructs import Construct
 
 from eoapi_cdk import StacBrowser
@@ -12,7 +13,7 @@ from eoapi_cdk import StacBrowser
 app = App()
 
 STAC_API_URL = os.environ["STAC_API_URL"]
-STAGE_NAME = os.environ["STAGE"]
+STAGE_NAME = os.environ["STAGE_NAME"]
 APP_NAME = os.environ["APP_NAME"]
 
 
@@ -26,14 +27,30 @@ class BrowserStack(Stack):
 
 browser_stack = BrowserStack(app, f"{APP_NAME}-{STAGE_NAME}")
 
+stac_browser_bucket = s3.Bucket(
+    browser_stack,
+    "stac-browser-bucket",
+    bucket_name=f"{APP_NAME}-{STAGE_NAME}",
+    removal_policy=RemovalPolicy.DESTROY,
+    auto_delete_objects=True,
+    website_index_document="index.html",
+    public_read_access=True,
+    block_public_access=s3.BlockPublicAccess(
+        block_public_acls=False,
+        block_public_policy=False,
+        ignore_public_acls=False,
+        restrict_public_buckets=False,
+    ),
+    object_ownership=s3.ObjectOwnership.OBJECT_WRITER,
+)
+
 stac_browser = StacBrowser(
     browser_stack,
     "stac-browser",
     github_repo_tag="v3.1.0",  # hard coded to the latest for now.
     stac_catalog_url=STAC_API_URL,  # using the non-custom-domain for now.
-    website_index_document="index.html",  # using simple static website hosting for now without a CloudFront distribution.
+    bucket_arn=stac_browser_bucket.bucket_arn,
 )
-stac_browser.bucket.grant_public_access()  # make it publicly accessible for the static website hosting to work.
 
 for key, value in {
     "Project": APP_NAME,
