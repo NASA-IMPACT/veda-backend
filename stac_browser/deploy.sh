5b19158945931e4e4c9256d9d73447b972e1f7f2
emileten <e.tenezakis@gmail.com>
2023-09-15 15:34:17 +0900

    revert d5ea0ee7f3340d78495991918c9662c7720f6833 and 6ccb839ab89f63ef6d3ba244c566b9ca8881f0b6

--- stac_browser/deploy.sh
+++
@@ -1,5 +0,0 @@
-export STAC_API_URL="https://xnv4kvaonc.execute-api.us-west-2.amazonaws.com/api/stac/"
-export STAGE_NAME="dev"
-export APP_NAME="veda-stac-browser"
-
-AWS_PROFILE=853558080719_AdministratorAccess cdk deploy --app "python3 app.py" --all --require-approval never
