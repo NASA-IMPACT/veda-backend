ead8e1470230c811b3a01659f20d526c01edaf5c
smohiudd <saadiq@developmentseed.org>
2023-09-28 10:07:43 -0600

    construct change

--- routes/infrastructure/construct.py
+++ routes/infrastructure/construct.py
@@ -29,30 +29,30 @@ class CloudfrontDistributionConstruct(Construct):
 
         stack_name = Stack.of(self).stack_name
 
-        s3Bucket = s3.Bucket.from_bucket_name(
-            self,
-            "stac-browser-bucket",
-            bucket_name=veda_route_settings.stac_browser_bucket,
-        )
-
-        no_cache_policy = cf.CachePolicy(
-            self,
-            "no-cache-policy",
-            default_ttl=Duration.seconds(0),
-            min_ttl=Duration.seconds(0),
-            max_ttl=Duration.seconds(0),
-        )
-
-        # Certificate must be in zone us-east-1
-        domain_cert = (
-            certificatemanager.Certificate.from_certificate_arn(
-                self, "domainCert", veda_route_settings.cert_arn
-            )
-            if veda_route_settings.cert_arn
-            else None
-        )
-
         if veda_route_settings.cloudfront:
+            s3Bucket = s3.Bucket.from_bucket_name(
+                self,
+                "stac-browser-bucket",
+                bucket_name=veda_route_settings.stac_browser_bucket,
+            )
+
+            no_cache_policy = cf.CachePolicy(
+                self,
+                "no-cache-policy",
+                default_ttl=Duration.seconds(0),
+                min_ttl=Duration.seconds(0),
+                max_ttl=Duration.seconds(0),
+            )
+
+            # Certificate must be in zone us-east-1
+            domain_cert = (
+                certificatemanager.Certificate.from_certificate_arn(
+                    self, "domainCert", veda_route_settings.cert_arn
+                )
+                if veda_route_settings.cert_arn
+                else None
+            )
+
             self.distribution = cf.Distribution(
                 self,
                 stack_name,
