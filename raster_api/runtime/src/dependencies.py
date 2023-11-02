ce9c7e89a0ee4315a6da67f7a0d80a09d497e743
vincentsarago <vincent.sarago@gmail.com>
2023-11-02 16:31:40 +0100

    add custom colormap for EPA dataset

--- raster_api/runtime/src/dependencies.py
+++ raster_api/runtime/src/dependencies.py
@@ -1,11 +1,19 @@
 """veda.raster.dependencies."""
 
 import pystac
+from rio_tiler.colormap import cmap as default_cmap
 from typing_extensions import Annotated
 
 from fastapi import Query
 from starlette.requests import Request
+from titiler.core.dependencies import create_colormap_dependency
 from titiler.pgstac.dependencies import get_stac_item
+
+try:
+    from importlib.resources import files as resources_files  # type: ignore
+except ImportError:
+    # Try backported to PY<39 `importlib_resources`.
+    from importlib_resources import files as resources_files  # type: ignore
 
 
 def ItemPathParams(
@@ -21,3 +29,10 @@ def ItemPathParams(
 ) -> pystac.Item:
     """STAC Item dependency."""
     return get_stac_item(request.app.state.dbpool, collection, item)
+
+
+VEDA_CMAPS_FILES = {
+    f.stem: str(f) for f in (resources_files(__package__) / "cmap_data").glob("*.npy")  # type: ignore
+}
+cmap = default_cmap.register(VEDA_CMAPS_FILES)
+ColorMapParams = create_colormap_dependency(cmap)
