8b3c6da77725730be6b740c7a6d47b3d1e1cce1f
Stephen Kilbourn <skilbourn@element84.com>
2024-03-08 10:46:33 -0700

    move schemas to json files

--- .github/workflows/tests/test_stac.py
+++ .github/workflows/tests/test_stac.py
@@ -1,124 +1,17 @@
 """test veda-backend STAC."""
 
+import json
+
 import httpx
 from openapi_schema_validator import validate
 
+with open(".github/workflows/tests/schemas/feature_schema.json", "r") as f:
+    feature_schema = json.load(f)
+
+with open(".github/workflows/tests/schemas/collection_schema.json", "r") as f:
+    collection_schema = json.load(f)
+
 stac_endpoint = "http://0.0.0.0:8081"
-
-
-feature_schema = {
-    "type": "object",
-    "required": ["id", "bbox", "type", "links", "stac_version", "stac_extensions"],
-    "properties": {
-        "id": {"type": "string"},
-        "bbox": {"type": "array"},
-        "type": {"type": "string"},
-        "links": {
-            "type": "array",
-            "items": {
-                "type": "object",
-                "properties": {
-                    "rel": {"type": "string"},
-                    "type": {"type": "string"},
-                    "href": {"type": "string"},
-                },
-            },
-        },
-        "assets": {"type": "object"},
-        "geometry": {
-            "type": "object",
-            "properties": {"type": {"type": "string", "coordinates": "array"}},
-        },
-        "collection": {"type": "string"},
-        "properties": {"type": "object"},
-        "stac_version": {"type": "string"},
-        "stac_extensions": {"type": "array"},
-    },
-}
-
-collection_schema = {
-    "type": "object",
-    "required": [
-        "stac_version",
-        # "type",
-        "id",
-        "description",
-        "license",
-        "extent",
-        "links",
-    ],
-    "properties": {
-        "stac_version": {"type": "string"},
-        "type": {"type": "string", "enum": ["Collection"]},
-        "id": {
-            "type": "string",
-        },
-        "description": {
-            "type": "string",
-            "description": "Detailed multi-line description to fully explain the catalog or collection.\n[CommonMark 0.29](http://commonmark.org/) syntax MAY be used for rich text representation.",
-        },
-        "keywords": {
-            "type": "array",
-            "description": "List of keywords describing the collection.",
-            "items": {"type": "string"},
-        },
-        "license": {"type": "string"},
-        "extent": {
-            "type": "object",
-            "required": ["spatial", "temporal"],
-            "properties": {
-                "spatial": {
-                    "type": "object",
-                    "required": ["bbox"],
-                    "properties": {
-                        "bbox": {
-                            "type": "array",
-                            "minItems": 1,
-                            "items": {
-                                "type": "array",
-                                "minItems": 4,
-                                "maxItems": 6,
-                                "items": {"type": "number"},
-                            },
-                        }
-                    },
-                },
-                "temporal": {
-                    "type": "object",
-                    "required": ["interval"],
-                    "properties": {
-                        "interval": {
-                            "type": "array",
-                            "minItems": 1,
-                            "items": {
-                                "type": "array",
-                                "minItems": 2,
-                                "maxItems": 2,
-                                "items": {
-                                    "type": "string",
-                                    "format": "date-time",
-                                    "nullable": "true",
-                                },
-                            },
-                        },
-                    },
-                },
-                "links": {
-                    "type": "array",
-                    "items": {
-                        "type": "object",
-                        "required": ["href", "rel"],
-                        "properties": {
-                            "href": {"type": "string", "format": "uri"},
-                            "rel": {"type": "string"},
-                        },
-                    },
-                },
-            },
-        },
-    },
-}
-
 
 def test_stac_api():
     """test stac."""
@@ -151,6 +44,7 @@ def test_stac_api():
     assert resp.status_code == 200
     item = resp.json()
     assert item["id"] == "20200307aC0853300w361200"
+    validate(item, feature_schema)
 
 
 def test_stac_to_raster():
