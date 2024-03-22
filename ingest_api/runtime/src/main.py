88b48a0439429e13ac100ba1e05b6336a73a6aed
ividito <isayah@developmentseed.org>
2024-03-22 09:42:45 -0300

    feat(otel): add otel to ingest API

--- ingest_api/runtime/src/main.py
+++ ingest_api/runtime/src/main.py
@@ -7,13 +7,16 @@ import src.config as config
 import src.dependencies as dependencies
 import src.schemas as schemas
 import src.services as services
+from aws_lambda_powertools.metrics import MetricUnit
 from src.collection_publisher import CollectionPublisher, ItemPublisher
 from src.doc import DESCRIPTION
+from src.monitoring import LoggerRouteHandler, logger, metrics, tracer
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import APIRouter, Depends, FastAPI, HTTPException
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from fastapi.security import OAuth2PasswordRequestForm
+from starlette.requests import Request
 
 settings = (
     config.Settings()
@@ -37,7 +40,9 @@ app = FastAPI(
     root_path=settings.root_path,
     openapi_url="/openapi.json",
     docs_url="/docs",
+    router=APIRouter(route_class=LoggerRouteHandler),
 )
+
 
 collection_publisher = CollectionPublisher()
 item_publisher = ItemPublisher()
@@ -222,7 +227,42 @@ def who_am_i(claims=Depends(auth.decode_token)):
     return claims
 
 
+# If the correlation header is used in the UI, we can analyze traces that originate from a given user or client
+@app.middleware("http")
+async def add_correlation_id(request: Request, call_next):
+    """Add correlation ids to all requests and subsequent logs/traces"""
+    # Get correlation id from X-Correlation-Id header if provided
+    corr_id = request.headers.get("x-correlation-id")
+    if not corr_id:
+        try:
+            # If empty, use request id from aws context
+            corr_id = request.scope["aws.context"].aws_request_id
+        except KeyError:
+            # If empty, use uuid
+            corr_id = "local"
+
+    # Add correlation id to logs
+    logger.set_correlation_id(corr_id)
+
+    # Add correlation id to traces
+    tracer.put_annotation(key="correlation_id", value=corr_id)
+
+    response = await tracer.capture_method(call_next)(request)
+    # Return correlation header in response
+    response.headers["X-Correlation-Id"] = corr_id
+    logger.info("Request completed")
+    return response
+
+
 # exception handling
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request, exc):
     return JSONResponse(str(exc), status_code=422)
+
+
+@app.exception_handler(Exception)
+async def general_exception_handler(request, err):
+    """Handle exceptions that aren't caught elsewhere"""
+    metrics.add_metric(name="UnhandledExceptions", unit=MetricUnit.Count, value=1)
+    logger.exception("Unhandled exception")
+    return JSONResponse(status_code=500, content={"detail": "Internal Server Error"})
