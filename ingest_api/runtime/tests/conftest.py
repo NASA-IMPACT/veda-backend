9a72ed66e90b3f9234d7cab2bca9680b368bd36d
Alexandra Kirk <alexandra@developmentseed.org>
2024-04-19 14:22:17 -0600

    mock cognito domain in ingest conftest

--- ingest_api/runtime/tests/conftest.py
+++ ingest_api/runtime/tests/conftest.py
@@ -28,6 +28,7 @@ def test_environ():
     os.environ["USERPOOL_ID"] = "fake_id"
     os.environ["STAGE"] = "testing"
     os.environ["ROOT_PATH"] = "/"
+    os.environ["COGNITO_DOMAIN"] = "https://test-cognito.url"
 
 
 @pytest.fixture
