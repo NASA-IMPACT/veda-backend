57daf5645151694498d29cb54d1996ffd13b8814
Jennifer Tran <tranjennif@gmail.com>
2024-05-02 13:16:00 -0700

    fix: remove None default

--- ingest_api/runtime/src/auth.py
+++ ingest_api/runtime/src/auth.py
@@ -56,7 +56,7 @@ def validated_token(
 
 def get_username(token: Annotated[Dict[Any, Any], Depends(validated_token)]) -> str:
     logger.info(f"\nToken {token}")
-    result = token["username"] if "username" in token else str(token.get("sub", None))
+    result = token["username"] if "username" in token else str(token.get("sub"))
     return result
 
 
