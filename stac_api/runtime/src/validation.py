5b1e2beb06f82e46bebfd54642d7e60ad4f8dde6
Slesa Adhikari <slesaad@gmail.com>
2024-05-22 13:54:24 -0500

    Update bulkitem model

--- stac_api/runtime/src/validation.py
+++ stac_api/runtime/src/validation.py
@@ -1,10 +1,20 @@
 import json
 import re
+from typing import Dict
 
 from fastapi import Request, HTTPException
-from pydantic import parse_obj_as, ValidationError
+from pydantic import ValidationError, BaseModel, Field
 from stac_pydantic import Item, Collection
 from starlette.middleware.base import BaseHTTPMiddleware
+
+
+class Items(BaseModel):
+    items: Dict[str, Item]
+
+
+class BulkItemsModel(BaseModel):
+    items: Items
+    method: str = Field(default="insert")
 
 
 class ValidationMiddleware(BaseHTTPMiddleware):
@@ -20,7 +30,7 @@ class ValidationMiddleware(BaseHTTPMiddleware):
                 ):
                     Item(**request_data)
                 elif re.match(r"^/collections/[^/]+/bulk-items$", request.url.path):
-                    parse_obj_as(list[Item], request_data)
+                    BulkItemsModel(**request_data)
             except (ValidationError, json.JSONDecodeError) as e:
                 raise HTTPException(status_code=400, detail=str(e))
 
