c55e375ca91c5bad20bc4bdcb5dfc6f29193b617
Slesa Adhikari <slesaad@gmail.com>
2024-05-28 10:31:45 -0500

    fix: make data access role optional in ingest runtime (#383)

--- ingest_api/runtime/src/config.py
+++ ingest_api/runtime/src/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
         description="URL of JWKS, e.g. https://cognito-idp.{region}.amazonaws.com/{userpool_id}/.well-known/jwks.json"  # noqa
     )
 
-    data_access_role_arn: AwsArn = Field(  # type: ignore
+    data_access_role_arn: Optional[AwsArn] = Field(  # type: ignore
         description="ARN of AWS Role used to validate access to S3 data"
     )
 
--- ingest_api/runtime/src/validators.py
+++ ingest_api/runtime/src/validators.py
@@ -9,8 +9,10 @@ import requests
 def get_s3_credentials():
     from src.main import settings
 
-    print("Fetching S3 Credentials...")
+    if not settings.data_access_role_arn:
+        return {}
 
+    print("Fetching S3 Credentials...")
     response = boto3.client("sts").assume_role(
         RoleArn=settings.data_access_role_arn,
         RoleSessionName="stac-ingestor-data-validation",
