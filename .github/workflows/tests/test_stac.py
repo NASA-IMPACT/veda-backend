80fb6c6d148ae5d5b6a1f89fbb926a8bd3ebf288
Stephen Kilbourn <skilbourn@element84.com>
2024-06-28 16:10:45 -0600

    initial round of stac tests

--- .github/workflows/tests/test_stac.py
+++ .github/workflows/tests/test_stac.py
@@ -2,16 +2,59 @@
 
 import httpx
 
+seeded_collection = "noaa-emergency-response"
 stac_endpoint = "http://0.0.0.0:8081"
+index_endpoint = "index.html"
+docs_endpoint = "docs"
+health_endpoint = "_mgmt/ping"
+search_endpoint = "search"
+
+
+def test_stac_health():
+    """test stac health endpoint."""
+    # Ping
+    assert httpx.get(f"{stac_endpoint}/{health_endpoint}").status_code == 200
+
+
+def test_stac_viewer():
+    """test stac viewer."""
+    resp = httpx.get(f"{stac_endpoint}/{index_endpoint}")
+    assert resp.status_code == 200
+    assert resp.headers.get("x-correlation-id") == "local"
+    assert "<title>Simple STAC API Viewer</title>" in resp.text
+
+
+def test_stac_docs():
+    """test stac docs."""
+    resp = httpx.get(f"{stac_endpoint}/{docs_endpoint}")
+    assert resp.status_code == 200
+    assert "Swagger UI" in resp.text
+
+
+def test_stac_get_search():
+    """test stac get search."""
+    default_limit_param = "?limit=10"
+    resp = httpx.get(f"{stac_endpoint}/{search_endpoint}{default_limit_param}")
+    assert resp.status_code == 200
+    features = resp.json()["features"]
+    assert len(features) > 0
+    collections = [c["collection"] for c in features]
+    assert seeded_collection in collections
+
+
+def test_stac_post_search():
+    """test stac post search."""
+    request_body = {"collections": [seeded_collection]}
+    resp = httpx.post(f"{stac_endpoint}/{search_endpoint}", json=request_body)
+    assert resp.status_code == 200
+    features = resp.json()["features"]
+    assert len(features) > 0
+    collections = [c["collection"] for c in features]
+    assert seeded_collection in collections
 
 
 def test_stac_api():
     """test stac."""
-    # Ping
-    assert httpx.get(f"{stac_endpoint}/_mgmt/ping").status_code == 200
-
-    # viewer
-    assert httpx.get(f"{stac_endpoint}/index.html").status_code == 200
 
     # Collections
     resp = httpx.get(f"{stac_endpoint}/collections")
