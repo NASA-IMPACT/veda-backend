95e76b09cedad05811777f9361512db55eb8d407
Slesa Adhikari <slesaad@gmail.com>
2024-07-30 10:08:24 -0500

    Format

--- stac_api/runtime/tests/conftest.py
+++ stac_api/runtime/tests/conftest.py
@@ -226,7 +226,9 @@ def test_environ():
     os.environ["VEDA_STAC_USERPOOL_ID"] = "us-west-2_FAKEUSERPOOL"
     os.environ["VEDA_STAC_CLIENT_ID"] = "Xdjkfghadsfkdsadfjas"
     os.environ["VEDA_STAC_CLIENT_SECRET"] = "dsakfjdsalfkjadslfjalksfj"
-    os.environ["VEDA_STAC_COGNITO_DOMAIN"] = "https://fake.auth.us-west-2.amazoncognito.com"
+    os.environ["VEDA_STAC_COGNITO_DOMAIN"] = (
+        "https://fake.auth.us-west-2.amazoncognito.com"
+    )
     os.environ["VEDA_STAC_ENABLE_TRANSACTIONS"] = "TRUE"
 
     # Config mocks
@@ -282,6 +284,7 @@ def api_client(app):
         TestClient: The TestClient instance for API testing.
     """
     from src.app import auth
+
     app.dependency_overrides[auth.validated_token] = override_validated_token
     yield TestClient(app)
     app.dependency_overrides.clear()
