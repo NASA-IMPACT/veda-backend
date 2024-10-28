2e79dcece0ed5235ebdcac8fb4c0fce2d607df0d
Alexandra Kirk <alexandra@developmentseed.org>
2024-10-28 16:51:04 -0600

    upgrade to stac-fastapi-pgstac

--- stac_api/runtime/setup.py
+++ stac_api/runtime/setup.py
@@ -8,10 +8,7 @@ with open("README.md") as f:
     long_description = f.read()
 
 inst_reqs = [
-    "stac-fastapi.api~=2.4",
-    "stac-fastapi.types~=2.4",
-    "stac-fastapi.extensions~=2.4",
-    "stac-fastapi.pgstac~=2.4",
+    "stac-fastapi-pgstac==2.5.0",
     "jinja2>=2.11.2,<4.0.0",
     "starlette-cramjam>=0.1.0.a0,<0.2",
     "importlib_resources>=1.1.0;python_version<='3.9'",  # https://github.com/cogeotiff/rio-tiler/pull/379
