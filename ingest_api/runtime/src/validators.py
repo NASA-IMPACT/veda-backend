e8f59273b89c968910d51c5143091afe2b1f97cf
Alexandra Kirk <alexandra@developmentseed.org>
2025-01-08 16:52:58 -0700

    cast stac url as string in collection exists validator

--- ingest_api/runtime/src/validators.py
+++ ingest_api/runtime/src/validators.py
@@ -90,7 +90,7 @@ def collection_exists(collection_id: str) -> bool:
     from src.main import settings
 
     url = "/".join(
-        f'{url.strip("/")}' for url in [settings.stac_url, "collections", collection_id]
+        f'{url.strip("/")}' for url in [str(settings.stac_url), "collections", collection_id]
     )
 
     if (response := requests.get(url)).ok:
