0d00b3bce2650276e62aeea544fa363921dfe063
ividito <isayah@developmentseed.org>
2025-07-17 10:42:35 -0300

    Try to fix issues with root path + viewer page

--- stac_api/runtime/src/app.py
+++ stac_api/runtime/src/app.py
@@ -130,7 +130,7 @@ if tiles_settings.titiler_endpoint:
 @app.get("/index.html", response_class=HTMLResponse)
 async def viewer_page(request: Request):
     """Search viewer."""
-    path = api_settings.root_path or ""
+    path = "/"
     return templates.TemplateResponse(
         "stac-viewer.html",
         {"request": request, "endpoint": str(request.url).replace("/index.html", path)},
