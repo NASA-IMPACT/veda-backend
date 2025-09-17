878382c3fa38f80fed71eb80fe737f9d7ad3b2ff
Gjore Milevski <dzole0311@gmail.com>
2025-09-17 17:53:57 +0200

    feat: add GRIB2 support to Raster API

--- raster_api/infrastructure/config.py
+++ raster_api/infrastructure/config.py
@@ -15,7 +15,7 @@ class vedaRasterSettings(BaseSettings):
     # For more information on GDAL env see: https://gdal.org/user/configoptions.html
     # or https://developmentseed.org/titiler/advanced/performance_tuning/
     env: Dict = {
-        "CPL_VSIL_CURL_ALLOWED_EXTENSIONS": ".tif,.TIF,.tiff",
+        "CPL_VSIL_CURL_ALLOWED_EXTENSIONS": ".tif,.TIF,.tiff,.grib2,.GRIB2",
         "GDAL_CACHEMAX": "200",  # 200 mb
         "GDAL_DISABLE_READDIR_ON_OPEN": "EMPTY_DIR",
         "GDAL_INGESTED_BYTES_AT_OPEN": "32768",
