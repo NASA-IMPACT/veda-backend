897ed9a2c55dd6d44a5bfb17681ad282eb6950fb
Anthony Lukach <anthonylukach@gmail.com>
2025-10-10 22:39:32 -0700

    Mv tests to integration tests

--- .github/workflows/tests/test_stac.py
+++ .github/workflows/tests/test_stac.py
@@ -1,4 +1,5 @@
 """test veda-backend STAC."""
+
 import httpx
 import pytest
 import yaml
@@ -156,3 +157,236 @@ class TestList:
             params={"assets": "cog"},
         )
         assert resp.status_code == 307
+
+
+class TestTenantFiltering:
+    """
+    Test cases for STAC API tenant filtering.
+
+    This class contains integration tests to ensure that tenant filtering works correctly
+    with the collections and items loaded in the test environment.
+    """
+
+    from conftest import TENANT_COLLECTIONS
+
+    @pytest.fixture(autouse=True)
+    def setup(
+        self,
+        collections_route,
+        search_route,
+        stac_endpoint,
+        tenant_collections,
+    ):
+        """
+        Set up the test environment with the required fixtures.
+        """
+        self.collections_route = collections_route
+        self.search_route = search_route
+        self.stac_endpoint = stac_endpoint
+        self.tenant_collections = tenant_collections
+
+    @pytest.mark.parametrize(
+        "tenant, tenant_collections",
+        [
+            (tenant, collections)
+            for tenant, collections in TENANT_COLLECTIONS.items()
+            if tenant
+        ],
+    )
+    def test_collections_listed_with_tenant(self, tenant, tenant_collections):
+        """Test that collections are listed when accessed with valid tenant."""
+        resp = httpx.get(f"{self.stac_endpoint}/{tenant}/collections")
+        assert resp.status_code == 200
+        assert set(c["id"] for c in resp.json()["collections"]) == set(
+            tenant_collections
+        )
+
+    def test_collections_listed_without_tenant(self):
+        """Test that collections are listed when accessed without tenant."""
+        resp = httpx.get(f"{self.stac_endpoint}/{self.collections_route}")
+        assert resp.status_code == 200
+        all_collections = set(
+            collection
+            for _tenant_collections in self.tenant_collections.values()
+            for collection in _tenant_collections
+        )
+        assert set(c["id"] for c in resp.json()["collections"]) == all_collections
+
+    def test_collections_not_listed_with_invalid_tenant(self):
+        """Test that no collections are listed when accessed with invalid tenant."""
+        resp = httpx.get(f"{self.stac_endpoint}/invalid-tenant/collections")
+        assert resp.status_code == 200
+        assert resp.json()["collections"] == []
+
+    @pytest.mark.parametrize(
+        "tenant, collection",
+        [
+            (tenant, collection)
+            for tenant, collections in TENANT_COLLECTIONS.items()
+            for collection in collections
+            if tenant
+        ],
+    )
+    def test_collection_available_with_tenant(self, tenant, collection):
+        """Test that a tenant's collection is available when accessed with valid tenant."""
+        resp = httpx.get(f"{self.stac_endpoint}/{tenant}/collections/{collection}")
+        assert resp.status_code == 200
+        assert resp.json()["id"] == collection
+
+    @pytest.mark.parametrize(
+        "collection",
+        set(
+            collection
+            for collections in TENANT_COLLECTIONS.values()
+            for collection in collections
+        ),
+    )
+    def test_collection_available_without_tenant(self, collection):
+        """Test that a specific collection is available when accessed without tenant."""
+        resp = httpx.get(f"{self.stac_endpoint}/{self.collections_route}/{collection}")
+        assert resp.status_code == 200
+        assert resp.json()["id"] == collection
+
+    @pytest.mark.parametrize(
+        "collection",
+        set(
+            collection
+            for collections in TENANT_COLLECTIONS.values()
+            for collection in collections
+        ),
+    )
+    def test_collection_not_available_with_invalid_tenant(self, collection):
+        """Test that a specific collection is not available when accessed with invalid tenant."""
+        resp = httpx.get(
+            f"{self.stac_endpoint}/invalid-tenant/collections/{collection}"
+        )
+        assert resp.status_code == 404
+        assert resp.json()["code"] == "NotFoundError"
+
+    @pytest.mark.parametrize(
+        "tenant, collection",
+        [
+            (tenant, collection)
+            for tenant, collections in TENANT_COLLECTIONS.items()
+            for collection in collections
+            if tenant
+        ],
+    )
+    def test_items_available_with_tenant(self, tenant, collection):
+        """Test that items are available when accessed with valid tenant."""
+        resp = httpx.get(
+            f"{self.stac_endpoint}/{tenant}/collections/{collection}/items"
+        )
+        assert resp.status_code == 200
+        items = resp.json()["features"]
+        assert len(items) > 0
+        # Should find items from the noaa-emergency-response collection
+        for item in items:
+            assert item["collection"] == collection
+
+    @pytest.mark.parametrize(
+        "collection",
+        set(
+            collection
+            for collections in TENANT_COLLECTIONS.values()
+            for collection in collections
+        ),
+    )
+    def test_items_available_without_tenant(self, collection):
+        """Test that items are available when accessed without tenant."""
+        resp = httpx.get(
+            f"{self.stac_endpoint}/{self.collections_route}/{collection}/items"
+        )
+        assert resp.status_code == 200
+        items = resp.json()["features"]
+        assert len(items) > 0
+        for item in items:
+            assert item["collection"] == collection
+
+    @pytest.mark.parametrize(
+        "collection",
+        set(
+            collection
+            for collections in TENANT_COLLECTIONS.values()
+            for collection in collections
+        ),
+    )
+    def test_items_not_available_with_invalid_tenant(self, collection):
+        """Test that no items are returned when accessed with invalid tenant."""
+        resp = httpx.get(
+            f"{self.stac_endpoint}/invalid-tenant/collections/{collection}/items"
+        )
+        assert resp.status_code == 200
+        items = resp.json()["features"]
+        assert len(items) == 0
+
+    @pytest.mark.parametrize(
+        "tenant,tenant_collections",
+        [
+            (tenant, collections)
+            for tenant, collections in TENANT_COLLECTIONS.items()
+            if tenant
+        ],
+    )
+    def test_search_with_tenant(self, tenant, tenant_collections):
+        """Searching with a tenant should return only items from that tenant's collections."""
+        resp = httpx.get(f"{self.stac_endpoint}/{tenant}/search")
+        assert resp.status_code == 200
+        items = resp.json()["features"]
+        assert len(items) > 0
+        for item in items:
+            assert item["collection"] in tenant_collections
+
+    def test_search_without_tenant(self):
+        """Searching without a tenant should return all collections, even those with no tenant."""
+        resp = httpx.get(
+            f"{self.stac_endpoint}/{self.search_route}", params={"limit": 1000}
+        )
+        assert resp.status_code == 200
+        items = resp.json()["features"]
+        assert len(items) > 0
+        # Should find items from all collections
+        assert set(item["collection"] for item in items) == set(
+            collection
+            for collections in self.tenant_collections.values()
+            for collection in collections
+        )
+
+    def test_search_not_available_with_invalid_tenant(self):
+        """Test that search returns no items when accessed with invalid tenant."""
+        resp = httpx.get(f"{self.stac_endpoint}/invalid-tenant/search")
+        assert resp.status_code == 200
+        items = resp.json()["features"]
+        assert len(items) == 0
+
+    @pytest.mark.parametrize(
+        "tenant",
+        [tenant for tenant in TENANT_COLLECTIONS.keys() if tenant],
+    )
+    def test_tenant_isolation(self, tenant):
+        """Test that tenants cannot access each other's collections."""
+        # emergency should not see barc-thomasfire
+        resp = httpx.get(f"{self.stac_endpoint}/{tenant}/collections")
+        assert resp.status_code == 200
+        other_tenants_collections = set(
+            collection
+            for tenant, collection in self.tenant_collections.items()
+            if tenant != tenant
+        )
+        forbidden_collections = set(other_tenants_collections) - set(
+            self.tenant_collections[tenant]
+        )
+        collections = resp.json()["collections"]
+        for collection in collections:
+            assert collection["id"] not in forbidden_collections
+
+        for collection in forbidden_collections:
+            resp = httpx.get(f"{self.stac_endpoint}/{tenant}/collections/{collection}")
+            assert resp.status_code == 404
+            assert resp.json()["code"] == "NotFoundError"
+
+            resp = httpx.get(
+                f"{self.stac_endpoint}/{tenant}/collections/{collection}/items"
+            )
+            assert resp.status_code == 200
+            assert resp.json()["features"] == []
