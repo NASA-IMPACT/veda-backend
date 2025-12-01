2a059b16fc0efdbfcb3fbf3b32aaba0a4787fd40
Jennifer Tran <12633533+botanical@users.noreply.github.com>
2025-12-01 10:51:26 -0800

    fix: upgrade httpx to match sap and update docker compose

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ extra_reqs = {
         "pytest",
         "pytest-asyncio",
         "pytest-cov",
-        "httpx==0.28.0",
+        "httpx[http2]>=0.28.0",
         "pypgstac==0.8.5",
         "psycopg[binary, pool]",
         "fastapi",
