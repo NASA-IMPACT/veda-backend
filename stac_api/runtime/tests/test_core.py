5985580f67ea23fc4f584481f32779ed005c0519
Sandra Hoang <sandrahoang686@gmail.com>
2026-03-16 13:49:55 -0400

    clean up test_core

--- stac_api/runtime/tests/test_core.py
+++ stac_api/runtime/tests/test_core.py
@@ -10,7 +10,6 @@ from src.core import VedaCrudClient
 from stac_fastapi.pgstac.core import CoreCrudClient
 from stac_fastapi.pgstac.types.search import PgstacSearch
 
-
 def make_client():
     """Create a VedaCrudClient instance without calling __init__."""
     return VedaCrudClient.__new__(VedaCrudClient)
