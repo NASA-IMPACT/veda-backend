daa86ef5852dd4ac75135607150c066c5f5ff2c7
ividito <isayah@developmentseed.org>
2026-03-24 09:51:44 -0700

    fix: pin titiler < 2

--- raster_api/runtime/setup.py
+++ raster_api/runtime/setup.py
@@ -10,15 +10,16 @@ inst_reqs = [
     # highest-tested versions of raster_api dependencies
     "rio-tiler>=7.0,<8.0",
     "titiler.pgstac==1.5.0",
-    # use highest available titiler based on rio-tiler and titiler-pgstac pins
-    "titiler.core<0.20",
-    "titiler.mosaic",
-    "titiler.extensions[cogeo]",
+    # use highest available titiler based on rio-tiler and titiler-pgstac pins (not 2.0 yet though)
+    "titiler.core<0.20,<2.0",
+    "titiler.mosaic<2.0",
+    "titiler.extensions[cogeo]<2.0",
     "starlette-cramjam>=0.3,<0.4",
     # based on AWS observability requirements
     "aws_xray_sdk>=2.6.0,<3",
     "aws-lambda-powertools>=1.18.0",
     "python-multipart==0.0.7",
+    "mangum<=0.19",
 ]
 
 extra_reqs = {
