d9860fca94bb3980bf501da7b77871c0e72c3308
Isayah Vidito <isayah@developmentseed.org>
2026-03-24 13:56:57 -0700

    fix: adjust lifespan handling (#577)

--- raster_api/runtime/handler.py
+++ raster_api/runtime/handler.py
@@ -17,17 +17,13 @@ logging.getLogger("mangum.lifespan").setLevel(logging.ERROR)
 logging.getLogger("mangum.http").setLevel(logging.ERROR)
 
 
-@app.on_event("startup")
-async def startup_event() -> None:
-    """Connect to database on startup."""
-    await connect_to_db(app, settings=settings.load_postgres_settings(), pool_kwargs={})
-
-
 handler = Mangum(app, lifespan="off", api_gateway_base_path=app.root_path)
 
 if "AWS_EXECUTION_ENV" in os.environ:
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(app.router.startup())
+    loop.run_until_complete(
+        connect_to_db(app, settings=settings.load_postgres_settings(), pool_kwargs={})
+    )
 
 # Add tracing
 handler.__name__ = "handler"  # tracer requires __name__ to be set
