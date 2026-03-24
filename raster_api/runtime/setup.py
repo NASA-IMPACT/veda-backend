e6963d414b8b45a3378b9454440c1dc18000f7fd
Isayah Vidito <isayah@developmentseed.org>
2026-03-24 10:06:40 -0700

    Apply suggestion from @vincentsarago
    Co-authored-by: Vincent Sarago <vincent.sarago@gmail.com>

--- raster_api/runtime/setup.py
+++ raster_api/runtime/setup.py
@@ -11,9 +11,9 @@ inst_reqs = [
     "rio-tiler>=7.0,<8.0",
     "titiler.pgstac==1.5.0",
     # use highest available titiler based on rio-tiler and titiler-pgstac pins (not 2.0 yet though)
-    "titiler.core<0.20,<2.0",
-    "titiler.mosaic<2.0",
-    "titiler.extensions[cogeo]<2.0",
+    "titiler.core<0.20",
+    "titiler.mosaic<0.20",
+    "titiler.extensions[cogeo]<0.20",
     "starlette-cramjam>=0.3,<0.4",
     # based on AWS observability requirements
     "aws_xray_sdk>=2.6.0,<3",
