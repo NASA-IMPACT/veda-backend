679e65ca1de87386b89701ccaca91c434a69e001
Jennifer Tran <12633533+botanical@users.noreply.github.com>
2026-05-15 12:23:23 -0700

    fix: bring in dev changes

--- raster_api/runtime/setup.py
+++ raster_api/runtime/setup.py
@@ -10,10 +10,10 @@ inst_reqs = [
     # highest-tested versions of raster_api dependencies
     "rio-tiler>=7.0,<8.0",
     "titiler.pgstac==1.5.0",
-    # use highest available titiler based on rio-tiler and titiler-pgstac pins
+    # use highest available titiler based on rio-tiler and titiler-pgstac pins (not 2.0 yet though)
     "titiler.core<0.20",
-    "titiler.mosaic",
-    "titiler.extensions[cogeo]",
+    "titiler.mosaic<0.20",
+    "titiler.extensions[cogeo]<0.20",
     "starlette-cramjam>=0.3,<0.4",
     # based on AWS observability requirements
     "aws_xray_sdk>=2.6.0,<3",
