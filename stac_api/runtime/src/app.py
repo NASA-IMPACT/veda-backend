efedde4d84b08e620a157e701a6e3c84bb1053a6
Jennifer Tran <12633533+botanical@users.noreply.github.com>
2026-05-22 10:39:17 -0700

    fix: remove CompressionMiddleware merge addition

--- stac_api/runtime/src/app.py
+++ stac_api/runtime/src/app.py
@@ -90,7 +90,6 @@ api = StacApi(
     items_get_request_model=items_get_request_model,
     response_class=ORJSONResponse,
     middlewares=[
-        Middleware(CompressionMiddleware),
         Middleware(ValidationMiddleware),
         Middleware(PrefixRedirectMiddleware),
     ],
