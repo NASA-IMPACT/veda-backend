5edff6d0f7969269b9cd6325e0d2d639e920267a
Jennifer Tran <12633533+botanical@users.noreply.github.com>
2026-05-27 13:39:09 -0700

    fix: pin starlette in stac api

--- stac_api/runtime/setup.py
+++ stac_api/runtime/setup.py
@@ -10,6 +10,7 @@ with open("README.md") as f:
 inst_reqs = [
     "boto3",
     "async-lru>=2.0.5",
+    "starlette==1.0.1",
     "stac-fastapi.api~=6.1",
     "stac-fastapi.types~=6.1",
     "stac-fastapi.extensions~=6.1",
