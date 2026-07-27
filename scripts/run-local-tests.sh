be7a57528060e4e122b34f569ddc8ec69d20aadc
Jamison French <jamison@developmentseed.org>
2026-07-27 12:12:43 -0500

    test: update tests

--- scripts/run-local-tests.sh
+++ scripts/run-local-tests.sh
@@ -54,8 +54,15 @@ uv run pytest .github/workflows/tests/ -vv -s
 
 # Run ingest unit tests
 echo "--- Running ingest api runtime tests ---"
-NO_PYDANTIC_SSM_SETTINGS=1 uv run --project ingest_api/runtime python -m pytest --cov=ingest_api/runtime/src ingest_api/runtime/tests/ -vv -s
+# Must ping PGSTAC_VERSION in multiple places due to version management outside of repository
+PGSTAC_VERSION=0.9.6 
+NO_PYDANTIC_SSM_SETTINGS=1 uv run --project ingest_api/runtime \
+    --with ./common/auth \
+    --with "pypgstac==${PGSTAC_VERSION}" \
+    pytest --cov=ingest_api/runtime/src ingest_api/runtime/tests/ -vv -s
 
 # Transactions tests
 echo "--- Running stac api runtime tests ---"
-uv run --project stac_api/runtime pytest stac_api/runtime/tests/ --asyncio-mode=auto -vv -s -p no:warnings
+uv run --project stac_api/runtime \
+    --with ./common/auth \
+    pytest stac_api/runtime/tests/ --asyncio-mode=auto -vv -s -p no:warnings
